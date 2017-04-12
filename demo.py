656cb581fa37491366b5111ca43f56b698741ea1
Siraj <sirajravel@gmail.com>
2017-04-12 09:55:32 -0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,12 +7,13 @@ import cPickle as pickle
 #even more environments
 import gym
 
-#Pong
+#Step 1 - How does pong work?
 #1 - receive image frame
 #2 - move paddle UP/DOWN? (binary)
 #3 - Make action recieve reward (+1 if moves past AI, -1 if missed ball, 0 otherwise)
 #lets forget about other pong details. focus on the algo.
 
+#Step 2 - On RL
 #1 RL is a Branch of machine learning concerned with taking #sequences of actions
 #Usually described in terms of agent interacting with a
 #previously unknown environment, trying to maximize cumulative reward
@@ -22,7 +23,20 @@ import gym
 #https://karpathy.github.io/assets/rl/policy.png
 #recieves image pixels, outputs probability of moving UP (stochasticity!)
 
+#Step 3  Policy gradients have 3 key differences from supervised learning
+#1 We dont have the correct labels so as a fake label we substitute the action we happened to sample from the policy 
+#2 We modulate the loss for each example multiplicatively based on the eventual outcome
+#since we want to increase the log probability for actions that worked and decrease it for those that didnt. 
+#3 runs on a continuously changing dataset (the episodes), scaled by the advantage, and we only want to do one (or very few) 
+#updates based on each sampled dataset.
+#4 can be used to backprop when nodes are stochastic (!!, more on this later)
 
+#Step 4 - PG vs Humans
+#Policy Gradients have to actually experience a positive reward, and experience it very often in order to eventually and slowly shift the policy parameters towards 
+#repeating moves that give high rewards. With our abstract model, humans can figure out what is likely to give rewards without ever actually experiencing the 
+#rewarding or unrewarding transition. I dont have to actually experience crashing my car into a wall a few hundred times before I slowly start avoiding to do so.
+
+#Step 5 - Credit Assignment problem
 #Credit assignment problem - we just got +1 reward. what caused it? millions parameters and pixels...which frame? 
 #no labels!
 #https://karpathy.github.io/assets/rl/rl.png
@@ -30,22 +44,7 @@ import gym
 #to update network via backprop
 #reward could be any positive number. doesnt matter. its about magnitude. thanks neural nets.
 
-
-#Policy gradients has 3 key differences from supervised learning
-#1) We don’t have the correct labels so as a “fake label” we substitute the action we happened to sample from the policy 
-#2) We modulate the loss for each example multiplicatively based on the eventual outcome, 
-#since we want to increase the log probability for actions that worked and decrease it for those that didn’t. 
-#3) runs on a continuously changing dataset (the episodes), scaled by the advantage, and we only want to do one (or very few) 
-#updates based on each sampled dataset.
-#4 can be used to backprop when nodes are stochastic (!!, more on this later)
-
-#PG vs Humans
-#Policy Gradients have to actually experience a positive reward, and experience it very often in order to eventually and slowly shift the policy parameters towards 
-#repeating moves that give high rewards. With our abstract model, humans can figure out what is likely to give rewards without ever actually experiencing the 
-#rewarding or unrewarding transition. I don’t have to actually experience crashing my car into a wall a few hundred times before I slowly start avoiding to do so.
-
 #Training time - 3 nights on a macbook, couple hours on GPU cluster in AWS
-
 
 
 # hyperparameters
@@ -62,7 +61,7 @@ if resume:
   model = pickle.load(open('save.p', 'rb')) #load from pickled checkpoint
 else:
   model = {} #initialize model 
-  #rand returns a sample (or samples) from the “standard normal” distribution
+  #rand returns a sample (or samples) from the standard normal distribution
   #xavier algo determines the scale of initialization based on the number of input and output neurons.
   #Imagine that your weights are initially very close to 0. What happens is that the signals shrink as it goes through each 
   #layer until it becomes too tiny to be useful. Now if your weights are too big, the signals grow at each layer 
@@ -72,6 +71,7 @@ else:
   model['W2'] = np.random.randn(H) / np.sqrt(H)
   #zeros like returns an array of zeros with the same shape and type as a given array.
   #we will update buffers that add up gradients over a batch
+  #where the model contains kv pairs, weights layers etc 
 grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } 
 ## rmsprop (gradient descent) memory used to update model
 rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } 
@@ -89,13 +89,15 @@ def prepro(I):
   I[I == 144] = 0 # erase background (background type 1)
   I[I == 109] = 0 # erase background (background type 2)
   I[I != 0] = 1 # everything else (paddles, ball) just set to 1
-  return I.astype(np.float).ravel()
+  return I.astype(np.float).ravel() #flattens 
 
 #In practice the discount factor could be used to model the fact that the decision maker is uncertain 
 #about if in the next decision instant the world (e.g., environment / game / process ) is going to end.
 #If the decision maker is a robot, the discount factor could be the probability that the robot is switched 
 #off in the next time instant (the world ends in the previous terminology). That is the reason why the robot 
 #is short sighted and does not optimize the sum reward but the discounted sum reward.
+
+#since we are otimizing for short term reward, not long term reward (like legend of zelda)
 def discount_rewards(r):
   """ take 1D float array of rewards and compute discounted reward """
   #initilize discount reward matrix as empty
@@ -169,6 +171,7 @@ while True:
   cur_x = prepro(observation)
   x = cur_x - prev_x if prev_x is not None else np.zeros(D)
   prev_x = cur_x
+  #so x is our image difference, feed it in!
 
   # forward the policy network and sample an action from the returned probability
   aprob, h = policy_forward(x)
@@ -194,25 +197,27 @@ while True:
     episode_number += 1
 
     # stack together all inputs, hidden states, action gradients, and rewards for this episode
-    epx = np.vstack(xs)
-    eph = np.vstack(hs)
-    epdlogp = np.vstack(dlogps)
-    epr = np.vstack(drs)
+    #each episode is a few dozen games
+    epx = np.vstack(xs) #obsveration
+    eph = np.vstack(hs) #hidden
+    epdlogp = np.vstack(dlogps) #gradient
+    epr = np.vstack(drs) #reward
     xs,hs,dlogps,drs = [],[],[],[] # reset array memory
 
     #the strength with which we encourage a sampled action is the weighted sum of all rewards afterwards, but later rewards are exponentially less important
-
     # compute the discounted reward backwards through time
     discounted_epr = discount_rewards(epr)
     # standardize the rewards to be unit normal (helps control the gradient estimator variance)
     discounted_epr -= np.mean(discounted_epr)
     discounted_epr /= np.std(discounted_epr)
 
+    #advatnage - quantity which describes how good the action is compared to the average of all the action.
     epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
     grad = policy_backward(eph, epdlogp)
     for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
 
     # perform rmsprop parameter update every batch_size episodes
+    #http://68.media.tumblr.com/2d50e380d8e943afdfd66554d70a84a1/tumblr_inline_o4gfjnL2xK1toi3ym_500.png
     if episode_number % batch_size == 0:
       for k,v in model.iteritems():
         g = grad_buffer[k] # gradient
